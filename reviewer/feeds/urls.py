e05a20c16ee94a209db922bc67b312230e4652b2
marat <marathagazeev@gmail.com>
2023-07-29 15:12:07 +0300

    Add search

--- reviewer/feeds/urls.py
+++ reviewer/feeds/urls.py
@@ -7,17 +7,26 @@ urlpatterns = [
     django.urls.path(
         '',
         feeds.views.NewMoviesView.as_view(),
+        name='new',
     ),
     django.urls.path(
         'new/',
         feeds.views.NewMoviesView.as_view(),
+        name='new',
     ),
     django.urls.path(
         'recently_added/',
         feeds.views.RecentlyAddedMoviesView.as_view(),
+        name='recently_added',
     ),
     django.urls.path(
         'best/',
         feeds.views.BestMoviesView.as_view(),
+        name='best',
+    ),
+    django.urls.path(
+        'search/',
+        feeds.views.SearchView.as_view(),
+        name='search',
     ),
 ]
--- reviewer/feeds/views.py
+++ reviewer/feeds/views.py
@@ -2,16 +2,30 @@ import core.feeds.views
 import movies.models
 
 
+class BestMoviesView(core.feeds.views.FeedBaseView):
+    feed_name = 'best'
+    queryset = movies.models.Movie.objects.get_best_movies()
+
+
 class NewMoviesView(core.feeds.views.FeedBaseView):
     feed_name = 'new'
     queryset = movies.models.Movie.objects.get_new_movies()
 
 
 class RecentlyAddedMoviesView(core.feeds.views.FeedBaseView):
-    feed_name = 'recently_added'
+    feed_name = 'recently added'
     queryset = movies.models.Movie.objects.get_recently_added_movies()
 
 
-class BestMoviesView(core.feeds.views.FeedBaseView):
-    feed_name = 'best'
-    queryset = movies.models.Movie.objects.get_best_movies()
+class SearchView(core.feeds.views.FeedBaseView):
+    feed_name = 'search'
+
+    def get_queryset(self):
+        return movies.models.Movie.objects.filter(
+            name__icontains=self.request.GET.get('q')
+        )
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['q'] = self.request.GET.get('q')
+        return context
