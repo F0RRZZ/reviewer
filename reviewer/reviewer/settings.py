31abef8eb7224034a68327d610a707189b9372cd
marat <marathagazeev@gmail.com>
2023-08-10 01:59:24 +0300

    Add docker-compose

--- reviewer/reviewer/settings.py
+++ reviewer/reviewer/settings.py
@@ -171,11 +171,9 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 
-CELERY_BROKER_URL = ''.join(['redis://', REDIS_HOST, ':', REDIS_PORT, '/0'])
+CELERY_BROKER_URL = f'redis://redis:{REDIS_PORT}'
 CELERY_BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
-CELERY_RESULT_BACKEND = ''.join(
-    ['redis://', REDIS_HOST, ':', REDIS_PORT, '/0']
-)
+CELERY_RESULT_BACKEND = f'redis://redis:{REDIS_PORT}'
 CELERY_ACCEPT_TOKEN = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
